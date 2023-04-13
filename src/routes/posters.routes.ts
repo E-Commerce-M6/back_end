7cda39115131ee6b4db4bf34acca3b9d330ca830
Guilherme Felipe <guilhermefelipecas@gmail.com>
2023-04-13 10:10:43 -0300

    feat: created validator for data and route for poster, its own schema, interface, service and controller, fix: app.ts, handle error after the routes

--- src/routes/posters.routes.ts
+++ src/routes/posters.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { ensureDataIsValidMiddleware } from "../middlewares";
+import { posterCreateSchema } from "../schemas/posters.schemas";
+import { createPosterController } from "../controllers/posters";
 
 const posterRoutes: Router = Router();
+
+posterRoutes.post("", ensureDataIsValidMiddleware(posterCreateSchema), createPosterController);
 
 export default posterRoutes;
