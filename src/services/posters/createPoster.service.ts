60a2f694f838e01fb339185ce49e5b206a22485e
Guilherme Felipe <guilhermefelipecas@gmail.com>
2023-04-24 11:35:46 -0300

    feat: user when create a poster

--- src/services/posters/createPoster.service.ts
+++ src/services/posters/createPoster.service.ts
@@ -5,10 +5,16 @@ import { IPosterCreateSchema, IPosterReturnSchema } from "../../interfaces/poste
 import { posterReturnSchema } from "../../schemas/posters.schemas";
 
 const createPosterService = async (
-  posterData: IPosterCreateSchema
+  posterData: IPosterCreateSchema,
+  userId: string
 ): Promise<IPosterReturnSchema> => {
   const posterRepository: Repository<Poster> = AppDataSource.getRepository(Poster);
-  const poster = posterRepository.create(posterData);
+  const poster = posterRepository.create({
+    user: {
+      id: userId,
+    },
+    ...posterData,
+  });
   await posterRepository.save(poster);
 
   const returnPoster = posterReturnSchema.parse(poster);
